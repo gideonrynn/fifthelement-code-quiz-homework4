d38502d960e48764dadd9c5aad8b74ef8cda1158
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-10 20:22:32 -0600

    created initial files, including index, images folder, questions.js, and updated readme

---
+++ assets/questions.js
@@ -0,0 +1,20 @@
+
+
+
+//* Store your questions as an array of objects in a separate file, `questions.js`, that follows this format: ```js//
+
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  ///etc.
+];
+
+//* The length of the array in `questions.js` determines the length of play. Fifteen seconds per question is a good estimate, so 5 questions will result in a length of play of 75 seconds.//
