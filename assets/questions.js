a19d1804bb16ea591636be6b6d86741d61fccb5f
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-12 21:09:42 -0600

    added starting text with start quiz button and nav bar with working timer and view scores text only

--- assets/questions.js
+++ assets/questions.js
@@ -1,5 +1,10 @@
+//All Variables
 
+//Timer Variables
 
+var timer = document.querySelector("#seconds");
+var progressUpdate = document.getElementById("progress");
+var timeRemaining = "0";
 
 //* Store your questions as an array of objects in a separate file, `questions.js`, that follows this format: ```js//
 
@@ -14,7 +19,44 @@ var questions = [
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
   },
-  ///etc.
+  {
+    title: "The statement is ended when you enter the _____",
+    choices: ["quotes", "semicolon", "parentheses", "square brackets"],
+    answer: "semicolon"
+  },
 ];
 
 //* The length of the array in `questions.js` determines the length of play. Fifteen seconds per question is a good estimate, so 5 questions will result in a length of play of 75 seconds.//
+
+
+//Timer Script (remember to rename these variables and whatnot)
+function setTime() {
+
+  var timeRemaining = "5";
+
+  var timerInterval = setInterval(function() {
+    timeRemaining--;
+    timer.textContent = timeRemaining;
+
+    if(timeRemaining === 0) {
+      clearInterval(timerInterval);
+      sendTimeIsUp();
+    }
+
+  }, 1000);
+}
+
+function sendTimeIsUp() {
+  timer.textContent = "0";
+
+  var timeIsUp = document.createElement("h1");
+  timeIsUp.innerHTML = "Time is up!";
+  progressUpdate.appendChild(timeIsUp);
+
+}
+
+startquizbtn.addEventListener("click", setTime);
+
+
+//End Timer Script
+
