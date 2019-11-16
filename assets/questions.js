291379d5a39ae2028e08ad9fb6cf9396b7405ff0
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-15 19:08:11 -0600

    updated questions and timer function

--- assets/questions.js
+++ assets/questions.js
@@ -1,41 +1,64 @@
-//All Variables
-
-//Timer Variables
-
-var timer = document.querySelector("#seconds");
-var progressUpdate = document.getElementById("progress");
-var timeRemaining = "0";
-
-//Object
-
-var answerList = document.querySelector("#answer-list");
-
-var starterSection = document.querySelector(".starter");
-
-var titlePlaceholder = document.querySelector(".title");
-
 //* Store your questions as an array of objects in a separate file, `questions.js`, that follows this format: ```js//
 
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "1. The Fifth Element was written by _______",
+    choices: ["John Stamos", "Luc Besson", "Sting", "Name 4"],
+    answer: "Luc Besson"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "2. Ruby Rhod is played by ________",
+    choices: ["Bruce Willis", "Chris Tucker", "Gary Oldman", "Milla Jovovich"],
+    answer: "Chris Tucker"
   },
   {
-    title: "The statement is ended when you enter the _____",
-    choices: ["quotes", "semicolon", "parentheses", "square brackets"],
-    answer: "semicolon"
+    title: "3. Zorg's middle name is ________",
+    choices: ["Jean", "Baptiste", "Emmanuel", "Zorg"],
+    answer: "Emmanuel"
+  },
+  {
+    title: "4. The fifth element is _______",
+    choices: ["love", "water", "earth", "wind"],
+    answer: "love"
+  },
+  {
+    title: "5. Milla Jovovich was __ years old when she landed the role of this movie.",
+    choices: ["19", "21", "33", "80"],
+    answer: "19"
   },
 ];
 
+//All Variables
+
+//Timer Variables
+var timer = document.querySelector("#seconds");
+var progressUpdate = document.getElementById("progress");
+var timeRemaining = "0";
+
+//Object Variables
+var answerList = document.querySelector("#answer-list");
+var starterSection = document.querySelector(".starter");
+var titlePlaceholder = document.querySelector(".title");
 
 //Timer Script (remember to rename these variables and whatnot)
+
+function addChoices() {
+
+  for (var i = 0; i < questions.length; i++) {
+
+  var li = document.createElement("li");
+  li.textContent = questions[i]["choices"];
+
+  var questionHeader = document.createElement("h5")
+  questionHeader.textContent = questions[i]["title"];
+  answerList.appendChild(li);
+  titlePlaceholder.appendChild(questionHeader);
+
+  
+
+  }
+}
+
 function setTime() {
 
   var timeRemaining = "5";
@@ -63,21 +86,6 @@ function sendTimeIsUp() {
 
 }
 
-function addChoices() {
-
-  for (var i = 0; i < questions[i].choices.length; i++) {
-
-  var li = document.createElement("li");
-  li.textContent = questions[i].choices;
-  li.setAttribute("data-index", i);
-
-  var questionHeader = document.createElement("h5")
-  questionHeader.textContent = questions[i]["title"];
-  answerList.appendChild(li);
-  titlePlaceholder.appendChild(questionHeader);
-
-  }
-}
 
 function hideStart() {
   starterSection.setAttribute('style', 'display:none');
@@ -86,6 +94,7 @@ function hideStart() {
 
 startquizbtn.addEventListener("click", setTime);
 startquizbtn.addEventListener("click", hideStart);
+
 
 //End Timer Script
 
