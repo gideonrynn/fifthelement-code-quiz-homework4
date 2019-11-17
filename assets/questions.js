9b62f0d5ccf14009b7aae11323f1c05e7a017f0e
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-16 21:19:04 -0600

    updated currentQustions

--- assets/questions.js
+++ assets/questions.js
@@ -7,7 +7,7 @@ var questions = [
     answer: "Luc Besson"
   },
   {
-    title: "Ruby Rhod is played by ________.",
+    title: "Ruby Rhod (character) is played by which actor/actress?",
     choices: ["Bruce Willis", "Chris Tucker", "Gary Oldman", "Milla Jovovich"],
     answer: "Chris Tucker"
   },
@@ -25,7 +25,13 @@ var questions = [
     title: "Milla Jovovich was __ years old when she landed the role of Leloo.",
     choices: ["19", "21", "33", "80"],
     answer: "19"
+  },
+  {
+    title: "Milla Jovovich was __ years old when she landed the role of Leloo.",
+    choices: ["19", "21", "33", "80"],
+    answer: "19"
   }
+
 ];
 
 //Set variables that will be updated by JavaScript through timer and click events
@@ -55,7 +61,9 @@ var userSelection = "";
 
 //variables that store clicks from the user
 
-//create function that will display the questions 
+
+//Question Function
+//The following will create function that will display the questions 
 function displayQuestions() {
 
   //set variable for the current question that we are on in the questions object
@@ -63,10 +71,11 @@ function displayQuestions() {
 
   titlePlaceholder.innerHTML = questions[currentQuestion].title;
 
-  //create for lop that will loop through each question 
-  for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+  //create for loop that will loop through each choice string in the object
+  for (var i = 0; i < question.choices.length; i++) {
 
     //create the list and button elements
+
     var li = document.createElement("li");
     
     //var button = document.createElement("button");
@@ -77,9 +86,10 @@ function displayQuestions() {
     
     //add unique identifiers to newly created li
     li.setAttribute("id", questions[currentQuestion].choices[i]);
+    li.setAttribute("class", "new");
 
     //append new button and new list item 
-    choicesList.appendChild(li); 
+    choicesList.append(li); 
     //li.appendChild(button);//
   }
 
@@ -89,15 +99,24 @@ function displayQuestions() {
 function chooseAnswer() {
   
   var choiceClicked = event.target.id;
+  var li = document.querySelectorAll("li");
 
   var currentAnswer = questions[currentQuestion].answer;
 
   if (choiceClicked === currentAnswer) {
-    progressUpdate.innerHTML = "Correct!";
+    progressUpdate.innerHTML = "Correct! +15 points";
+    timeRemaining = +15;
 } 
   else {
-    progressUpdate.innerHTML = "Wrong!";
+    progressUpdate.innerHTML = "Wrong! -15 points";
   }
+
+  if (timeRemaining > 0) {
+    currentQuestion = +currentQuestion + 1;
+    progressUpdate.innerHTML = "";
+  }
+
+  displayQuestions();
 }
 
 
@@ -119,19 +138,15 @@ function setTime() {
 
   }, 1000);
 
-  displayQuestions()
 }
 
 function sendTimeIsUp() {
   timer.textContent = "0";
-
-  var timeIsUp = document.createElement("h5");
-  timeIsUp.innerHTML = ("<hr>" + "Time is up!");
-  progressUpdate.appendChild(timeIsUp);
+  progressUpdate.innerHTML = ("Time is up!");
 
 }
 
-//hide the Starter section by changing the style-> display on the start section div
+//hide the Starter section by changing the style: display on the start section div
 function hideStart() {
   starterSection.setAttribute('style', 'display:none');
 }
@@ -139,4 +154,5 @@ function hideStart() {
 //kick off timer and hide the starter div with click events connected to the Start Button
 startquizbtn.addEventListener("click", setTime);
 startquizbtn.addEventListener("click", hideStart);
+startquizbtn.addEventListener("click", displayQuestions);
 choicesList.addEventListener("click", chooseAnswer);
